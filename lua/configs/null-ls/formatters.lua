0727847f4b6ef4238bf96a9fd2ef53e22aeed857
0uJin <oujin@helps.show>
2023-05-11 15:40:37 +0800

    chore: 更改pep8每行最大的字符数

--- lua/configs/null-ls/formatters.lua
+++ lua/configs/null-ls/formatters.lua
@@ -3,7 +3,9 @@ local elements = {}
 
 elements.python = {
     filetypes = { "python" },
-    null_ls.builtins.formatting.autopep8,
+    null_ls.builtins.formatting.autopep8.with({
+        extra_args = { "--max-line-length=120" }
+    }),
 }
 
 -- ================================================ 返回通用元素 ================================================
