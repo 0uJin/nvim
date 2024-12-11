a3d03d96f663b38b3fcb620081d7c698f160eaec
欧锦 <oujin@Aisaka-Mac-mini.local>
2024-12-11 08:38:32 +0800

    feat: <nui>新增插件管理菜单

--- lua/configs/nui/keymaps.lua
+++ lua/configs/nui/keymaps.lua
@@ -5,3 +5,5 @@ local noremap = keymaps.noremap
 -- -------------------------------------------------- 基础按键 --------------------------------------------------
 -- <Alt> + b, 打开代码编程工具弹窗
 noremap('n', '<A-b>', '<cmd> lua require("configs.nui.code-menu").open() <CR>')
+-- <Alt> + p, 打开插件管理弹窗
+noremap('n', '<A-p>', '<cmd> lua require("configs.nui.plugins-menu").open() <CR>')
