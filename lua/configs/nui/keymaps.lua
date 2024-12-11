ea509d1dacf8a3f4534919932c2e6ccbdecbd71f
欧锦 <oujin@Aisaka-Mac-mini.local>
2024-12-11 08:37:45 +0800

    chore: <nui>代码工具菜单调整

--- lua/configs/nui/keymaps.lua
+++ lua/configs/nui/keymaps.lua
@@ -3,6 +3,5 @@ local keymaps = require('custom.keymaps')
 local noremap = keymaps.noremap
 
 -- -------------------------------------------------- 基础按键 --------------------------------------------------
--- <Alt> + b, 打开工具弹窗
-noremap('n', '<A-b>', '<cmd> lua require("configs.nui.menu").open() <CR>')
-
+-- <Alt> + b, 打开代码编程工具弹窗
+noremap('n', '<A-b>', '<cmd> lua require("configs.nui.code-menu").open() <CR>')
